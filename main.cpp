3213d4a32ed69d95ea1e99981e584477154b2c1b
Anas <anasamburulloh17@gmail.com>
2026-06-10 23:41:05 +0700

    pass admin

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <string>
-
+#include <ctime>
+#include <fstream>
 using namespace std;
 struct admin
 {
     string username;
     string password;
 };
-
+admin daftarAdmin[6] = {
+        {"Solose", "Solose17012007"},
+        {"Cayla", "Cayla123"},
+        {"Dicky", "Dicky123"}, 
+        {"Tiara", "Tiara123"},
+        {"Putri", "Putri123"},
+        {"siapalah", "okgasokgasprabowogibranoranggas"}
+    };
 void mainmenu(){
     int pilihan;
 
     cout << "==================================================\n";
     cout << "          SELAMAT DATANG DI STUDIO FOTO           \n";
     cout << "==================================================\n";
-    cout << "1. Sign In (Masuk Akun)\n";
-    cout << "2. Sign Up (Daftar Akun Baru)\n";
+    cout << "1. Admin \n";
+    cout << "2. Visitor\n";
     cout << "3. Keluar\n";
     cout << "==================================================\n";
     cout << "Pilih opsi (1-3): ";
